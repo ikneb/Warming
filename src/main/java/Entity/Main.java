4317ecb436560f4385e620f14e10a6cbcca1e668
ikneb <ikneblis@gmail.com>
2016-04-03 23:50:29 +0300

    add servlet and entity but it don't work

--- src/main/java/Entity/Main.java
+++ src/main/java/Entity/Main.java
@@ -1,30 +1,23 @@
 package Entity;
 
 import Dao.CustomerDao;
-
+import Dao.SmsDao;
 import general.Factory;
-
 import org.json.simple.JSONObject;
 import table.Customer;
+import table.Sms;
 import unlimit.MyServlet;
-
-
+import unlimit.MyServletSms;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * Created by Benki on 18.02.2016.
- */
 public class Main {
 
     double price;
 
-
     public  void recordDB() {
-
         int warmQuantity;
         int glueQuantity;
         int puttiQuantity;
@@ -37,7 +30,6 @@ public class Main {
         gridQuantity = QuantityMaterial.qridQuantity(MyServlet.quad);
         paintQuantity = QuantityMaterial.paintQuantity(MyServlet.quad);
 
-
         Factory factory = Factory.getInstance();
         CustomerDao customerDao = factory.getCustomerDao();
 
@@ -49,20 +41,13 @@ public class Main {
         customer.setMaterial(MyServlet.material);
         customer.setDensity(MyServlet.density);
 
-
         //add in data base
         try {
             customerDao.addCustomer(customer);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
-
-
-
     }
-
 
     public static double calkPrice() {
         String name;
@@ -72,47 +57,33 @@ public class Main {
         int quadrature;
         double result = 0;
 
-
         //выщитываем стоимость
-
         thick = MyServlet.material;
 
         if (thick.equals("styrofoam")) {
-
             int density = MyServlet.density;                           //плотность
             depth = MyServlet.thick;
             quadrature = MyServlet.quad;
             Styrofoam styrofoam = new Styrofoam();
             result = styrofoam.price(quadrature, depth, density);
-
-
         } else if (thick.equals("expanded_polystyrene")) {
             depth = MyServlet.thick;
             quadrature = MyServlet.quad;
             ExtrudedPolystyrene extrudedPolystyrene = new ExtrudedPolystyrene();
             result = extrudedPolystyrene.price(quadrature, depth);
-
         } else if (thick.equals("mineral_wool")) {
-
             depth = MyServlet.thick;
             quadrature = MyServlet.quad;
-
             MineralWool mineralWool = new MineralWool();
             result = mineralWool.price(quadrature, depth);
         }
-
-
         return result;
-
     }
 
     public  JSONObject allall() {
-
         List<Customer> customers = new ArrayList<>();
-
         JSONObject obj = new JSONObject();
         JSONObject resultJson = new JSONObject();
-
         Factory factory = Factory.getInstance();
         CustomerDao customerDao = factory.getCustomerDao();
 
@@ -133,13 +104,26 @@ public class Main {
             resultJson.put("id"+customer.getId(), obj);
 
         }
-
         System.out.println(resultJson);
-
-
-
         return resultJson;
+    }
 
+    public static void sms(){
+        Factory factory = Factory.getInstance();
+        SmsDao smsDao = factory.getSmsDao();
+
+        Sms sms = new Sms();
+        sms.setName(MyServletSms.name);
+        sms.setEmail(MyServletSms.email);
+        sms.setNumber(MyServletSms.number);
+        sms.setSms(MyServletSms.sms);
+
+        //add in data base
+        try {
+            smsDao.addSms(sms);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     }
--- src/main/java/unlimit/MyServletSms.java
+++ src/main/java/unlimit/MyServletSms.java
@@ -1,7 +1,43 @@
 package unlimit;
 
-/**
- * Created by Benki on 03.04.2016.
- */
-public class MyServletSms {
+import Entity.Main;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/MyServletSms")
+public class MyServletSms extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    public static String name;
+    public static String email;
+    public static int number;
+    public static String sms;
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        name = request.getParameter("name");
+        email = request.getParameter("email");
+        number = Integer.parseInt(request.getParameter("number"));
+        sms = request.getParameter("sms");
+
+
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("ok");
+        out.close();
+
+
+
+
+    }
 }
